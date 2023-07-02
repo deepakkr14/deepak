ad7290fdbac42f008848ec3c62598d1f7409f70b
deepakkr14 <deepakbipin142@gmail.com>
2023-07-02 22:25:50 +0530

    clean up code

--- apps.js
+++ apps.js
@@ -1,37 +1,4 @@
 const http = require("http");
-const fs = require("fs");
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  //   console.log(req);
-  if (url === "/") {
-    const messages = fs.readFileSync("message.txt", "utf8");
-    // const messageList = messages.split("\n").filter((message) => message.trim() !== "");
-    res.write("<html>");
-    res.write("<head><title>My Page</title></head>");
-    res.write("<body>");
-    res.write(messages);
-    res.write('<form action="/message" method="POST">');
-    res.write('<input type="text" name="message">');
-    res.write('<button type="submit">Send</button>');
-    res.write("</form>");
-    res.write("</body>");
-    res.write("</html>");
-
-    return res.end();
-  } else if (url === "/message" && method == "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message);
-      return res.end();
-    });
-  }
-});
+const mod=require("./shortcut")
+const server = http.createServer(mod.handler);
 server.listen(8004);
