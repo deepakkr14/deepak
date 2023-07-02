0c26195af2fa7d21fa2f9fb20ab9ebeb06cbdc77
deepakkr14 <deepakbipin142@gmail.com>
2023-07-02 22:28:38 +0530

    clean code 2

--- apps.js
+++ apps.js
@@ -1,4 +1,51 @@
-const http = require("http");
-const mod=require("./shortcut")
-const server = http.createServer(mod.handler);
-server.listen(8004);
+// const http = require("http");
+// const mod=require("./shortcut")
+// const server = http.createServer(mod.handler);
+// server.listen(8004);
+
+const fs = require("fs");
+
+const main= (req,res)=>{
+    const url = req.url;
+  const method = req.method;
+  
+    if (url === "/") {
+        const messages = fs.readFileSync("message.txt", "utf8");
+        res.write("<html>");
+        res.write("<head><title>My Page</title></head>");
+        res.write("<body>");
+        res.write(messages);
+        res.write('<form action="/message" method="POST">');
+        res.write('<input type="text" name="message">');
+        res.write('<button type="submit">Send</button>');
+        res.write("</form>");
+        res.write("</body>");
+        res.write("</html>");
+    
+        return res.end();
+      } else if (url === "/message" && method == "POST") {
+        const body = [];
+        req.on("data", (chunk) => {
+          body.push(chunk);
+        });
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        req.on("end", () => {
+          const parsedBody = Buffer.concat(body).toString();
+          const message = parsedBody.split("=")[1];
+          fs.writeFileSync("message.txt", message);
+          return res.end();
+        });
+      }
+    
+
+}
+
+// module.exports={    handler:main,
+//     sometext:"thi s is text"
+// }
+
+// module.exports.handler=main;
+exports.handler=main;
+
+// module.exports=main;
