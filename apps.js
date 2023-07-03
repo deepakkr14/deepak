77f5034b3522c3a713758a2f83fb0fa91491d2fb
deepakkr14 <deepakbipin142@gmail.com>
2023-07-03 22:59:37 +0530

    express

--- apps.js
+++ apps.js
@@ -1,21 +1,17 @@
 const http = require("http");
 const express = require("express");
-const chat =express();
-chat.use((req,res,next)=>{
-    console.log("in the middleware");
-    next();
+const chat = express();
+chat.use((req, res, next) => {
+  console.log("in the middleware");
+  next();
 });
 
-chat.use((req,res,next)=>{
-    console.log("in the 2nd  middleware");
-    res.send("<h7> heppy birthday</h7>")
+chat.use((req, res, next) => {
+  console.log("in the 2nd  middleware");
+  res.send("<h7> heppy birthday</h7>");
 });
 
-
-
-
-// const mod=require("./shortcut")
 const server = http.createServer(chat);
-server.listen(8003,()=>{
-    console.log("server is running in the background")
+server.listen(8003, () => {
+  console.log("server is running in the background");
 });
