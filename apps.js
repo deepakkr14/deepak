c2937b965a40769f4298bf98af474fafcb2d1fa3
deepakkr14 <deepakbipin142@gmail.com>
2023-07-03 22:59:13 +0530

    express

--- apps.js
+++ apps.js
@@ -1,51 +1,21 @@
-// const http = require("http");
+const http = require("http");
+const express = require("express");
+const chat =express();
+chat.use((req,res,next)=>{
+    console.log("in the middleware");
+    next();
+});
+
+chat.use((req,res,next)=>{
+    console.log("in the 2nd  middleware");
+    res.send("<h7> heppy birthday</h7>")
+});
+
+
+
+
 // const mod=require("./shortcut")
-// const server = http.createServer(mod.handler);
-// server.listen(8004);
-
-const fs = require("fs");
-
-const main= (req,res)=>{
-    const url = req.url;
-  const method = req.method;
-  
-    if (url === "/") {
-        const messages = fs.readFileSync("message.txt", "utf8");
-        res.write("<html>");
-        res.write("<head><title>My Page</title></head>");
-        res.write("<body>");
-        res.write(messages);
-        res.write('<form action="/message" method="POST">');
-        res.write('<input type="text" name="message">');
-        res.write('<button type="submit">Send</button>');
-        res.write("</form>");
-        res.write("</body>");
-        res.write("</html>");
-    
-        return res.end();
-      } else if (url === "/message" && method == "POST") {
-        const body = [];
-        req.on("data", (chunk) => {
-          body.push(chunk);
-        });
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        req.on("end", () => {
-          const parsedBody = Buffer.concat(body).toString();
-          const message = parsedBody.split("=")[1];
-          fs.writeFileSync("message.txt", message);
-          return res.end();
-        });
-      }
-    
-
-}
-
-// module.exports={    handler:main,
-//     sometext:"thi s is text"
-// }
-
-// module.exports.handler=main;
-exports.handler=main;
-
-// module.exports=main;
+const server = http.createServer(chat);
+server.listen(8003,()=>{
+    console.log("server is running in the background")
+});
