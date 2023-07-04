1336f1d91e0d630c3c5814be54682a2e4d807285
deepakkr14 <deepakbipin142@gmail.com>
2023-07-04 21:06:07 +0530

    body parser

--- apps.js
+++ apps.js
@@ -1,16 +1,27 @@
 const http = require("http");
 const express = require("express");
 const chat = express();
-chat.use((req, res, next) => {
-  console.log("in the middleware");
+chat.use(express.json());
+
+// Parse URL-encoded bodies
+chat.use(express.urlencoded({ extended: true }));
+chat.use("/add-product", (req, res, next) => {
+  res.send(
+    `<form action="/product"method="POST"><input type="text" name ="title"><input type="text" name ="title2"><button>send</button> </form>`
+  );
+});
+
+chat.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+chat.use("/", (req, res, next) => {
+//   console.log("in the 2nd  middleware");
   next();
 });
 
-chat.use((req, res, next) => {
-  console.log("in the 2nd  middleware");
-  res.send("<h7> heppy birthday</h7>");
-});
-
+// const mod=require("./shortcut")
 const server = http.createServer(chat);
 server.listen(8003, () => {
   console.log("server is running in the background");
