c00469bcd2faf876f8915d4cb036fccbf01f731b
deepakkr14 <deepakbipin142@gmail.com>
2023-08-05 22:34:11 +0530

    task 21

--- task 18/app.js
+++ task 18/app.js
@@ -7,6 +7,8 @@ const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -39,6 +41,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart)
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through:CartItem})
+Product.belongsToMany(Cart,{through:CartItem})
 
 sequelize
   //   .sync({ force: true })
@@ -56,6 +62,9 @@ sequelize
   .then((user) => {
     // console.log(user))
     // .catch(err=>{console.log(err)})
+     user.createCart();
+   
+  }).then(cart=>{
     app.listen(3000);
   })
   //   })
